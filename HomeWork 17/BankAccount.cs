efca0a7e2dbdcf1a97ea3b49e522c70347ffaec1
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-12-04 17:17:12 +0300

    Добавьте файлы проекта.

---
+++ HomeWork 17/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_17
+{
+    class BankAccount<T>
+    {
+        private string lastName;
+        private string firstName;
+        private string middleName;
+        private T numberAccount;
+        private decimal balance;
+        public void Record(string lastName, string firstName, string middleName, T numberAccount, decimal balance)      //записывает данные счета в объект
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.numberAccount = numberAccount;
+            this.balance = balance;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"\nДанные счета:\nФамилия:{lastName}\nИмя:{firstName}\nОтчество:{middleName}\nНомер счета:{numberAccount}\nТекущий баланс:{balance:f2}");
+        }       //выводит данные счета на консоль
+        /*public void BalanceChanger()
+        {
+            Console.WriteLine("Если вы хотите пополнить счет нажмите '+' , для снятия денег со счета нажмите '-' ");
+            ConsoleKey consolekey = Console.ReadKey().Key;
+            decimal value;
+            while (true)
+            {
+                switch (consolekey)
+                {
+                    case ConsoleKey.Add:
+                        value = ReadDecValue("Введите сумму пополнения:");
+                        balance += value;
+                        Console.WriteLine();
+                        break;
+                    case ConsoleKey.Subtract:
+                        value = ReadDecValue("Введите сумму снятия:");
+                        balance -= value;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        break;
+                }
+            }
+        }*/
+        public void BalanceAdd(decimal value)        //пополнение счета
+        {
+            balance += value;
+        }
+
+        public void BalanceSub(decimal value)        //снятие со счета
+        {
+            balance -= value;
+        }        
+    }
+}
---
+++ HomeWork 17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_17
+{
+    //Создать класс для моделирования счета в банке.Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+    //Класс должен быть объявлен как обобщенный.Универсальный параметр T должен определять тип номера счета.
+    //Разработать методы  для доступа  к данным  –  заполнения и  чтения.
+    //Создать несколько экземпляров класса, параметризированного различными типам.
+    //Заполнить его поля и вывести информацию об экземпляре класса на печать.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<long> account1 = new BankAccount<long>();
+            BankAccount<string> account2 = new BankAccount<string>();
+            account1.Print();
+            account2.Print();
+            account1.Record("Иванов", "Иван", "Иванович", 4815162342, (decimal)323453.43);
+            account2.Record("Сидорова", "Анна", "Николаевна", "DS34523484" , (decimal)0.45);
+            account1.Print();
+            account2.Print();
+            account1.BalanceSub(200000);
+            account2.BalanceAdd(200000);
+            account1.Print();
+            account2.Print();
+            Console.ReadKey();
+        }        
+    }
+    
+
+}
